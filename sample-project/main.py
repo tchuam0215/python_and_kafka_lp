3306b1c221e55a3f15fdd81443d32a467a2e2650
Tchuenkam Charles <ch.tchuenkam@gmail.com>
2023-09-13 13:58:26 +0200

    simple workflow example for python

--- sample-project/main.py
+++ sample-project/main.py
@@ -1,3 +1,19 @@
 import requests 
-r = requests.get('https://manning.com')
-print(r)
+
+url = 'https://manning.com'
+#url = 'https://api.github.com'
+#url = 'https://httpbin.org/cookies'
+#url = 'https://httpbin.org'
+
+# send an http get request to the url
+response = requests.get(url)
+
+if response.status_code == 200 :
+    print(response.text)    
+    #print(response.headers['content-type'])
+    #print(response.encoding)
+    #print(response.text)
+    #print(response.json())
+
+else : 
+    print(f"Failed to retrieve the website. Status code: {response.status_code}")
